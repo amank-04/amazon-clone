6ffc147c73b2df67292d9ad096003cd9a86bb5f3
dev <113306065+dev4lov@users.noreply.github.com>
2023-09-22 18:07:06 +0530

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -3,12 +3,12 @@ import firebase from "firebase/compat/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: XXXX,
-  authDomain: XXXX,
-  projectId: XXXX,
-  storageBucket: XXXX,
-  messagingSenderId: XXXX,
-  appId: XXXX,
+  apiKey: "AIzaSyDQv4wagmT5AnAGu0lGBLCnhshdIyEX9V0",
+  authDomain: "clone-398311.firebaseapp.com",
+  projectId: "amazon-clone-398311",
+  storageBucket: "amazon-clone-398311.appspot.com",
+  messagingSenderId: "610593707379",
+  appId: "1:610593707379:web:b5d69d3d5c1d42956d2106",
 };
 
 // Initialize Firebase
